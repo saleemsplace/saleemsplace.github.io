03bac1eb5fdd192150e7d96ac61eda2bc1442d8b
saleem <saleemsplace@aol.com>
2021-09-06 18:41:08 -0700

    test

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-const nav = document.querySelector(".nav");
-window.addEventListener("scroll", () => {
-  fixNav();
-});
+const left = document.querySelector('.left')
+const right = document.querySelector('.right')
+const container= document.querySelector('.container')
 
-function fixNav() {
-  if (window.scrollY > nav.offsetHeight + 150) {
-    nav.classList.add("active");
-  } else {
-    nav.classList.remove("active");
-  }
-}
+
+left.addEventListener('mouseenter',() => container.classList.add('hover-left'))
+left.addEventListener('mouseleave',() => container.classList.remove('hover-left'))
+
+right.addEventListener('mouseenter',() => container.classList.add('hover-right'))
+right.addEventListener('mouseleave',() => container.classList.remove('hover-right'))
